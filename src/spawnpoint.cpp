21605bf3c6999ad1eb210b86b23af104cd85ff64
CristianVictorZ <148216241+CristianVictorZ@users.noreply.github.com>
2024-01-12 08:28:23 +0200

    Update spawnpoint.cpp

--- src/spawnpoint.cpp
+++ src/spawnpoint.cpp
@@ -1,36 +1,38 @@
 #include "../headers/spawnpoint.hpp"
 
+///Constructor
 SpawnPoint::SpawnPoint(sf::Vector2f _position, const std::vector<sf::Vector2f> &_checkpoints, sf::Time _spawnTime, int _enemyNumber, int _enemyType) :
             spawnTime(_spawnTime), position(_position), checkpoints(_checkpoints), enemyType(_enemyType), enemyNumber(_enemyNumber)
             {generatedEnemies = 0;}
 
+///Destructor
 SpawnPoint::~SpawnPoint(){}
 
+///Intoarce pozitia unui spawnpoint
 sf::Vector2f SpawnPoint::getSpawnPointPosition()
 {
     return position;
 }
 
+///Intoarce vectorul de checkpoint-uri pentru un spawnpoint.
 std::vector<sf::Vector2f> SpawnPoint::getCheckpoints()
 {
     return checkpoints;
 }
 
+///Intoarce numarul de inamici total pe care il genereaza un spawnpoint.
 int SpawnPoint::getEnemyNumber()
 {
     return enemyNumber;
 }
 
+///Intoarce tipul de inamic generat de un spawnpoint.
 int SpawnPoint::getEnemyType()
 {
     return enemyType;
 }
 
-//    int SpawnPoint::getGeneratedEnemies()
-//    {
-//        return generatedEnemies;
-//    }
-
+///Verifica daca un spawnpoint poate genera un inamic.
 bool SpawnPoint::canGenerateEnemy()
 {
     sf::Time elapsedTime = spawnTimer.getElapsedTime();
