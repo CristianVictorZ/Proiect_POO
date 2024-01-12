6abf460f63584ee8f8fd907d42e5ec2b873ba47c
CristianVictorZ <148216241+CristianVictorZ@users.noreply.github.com>
2024-01-12 08:25:38 +0200

    Update exceptii.cpp

--- src/exceptii.cpp
+++ src/exceptii.cpp
@@ -1,5 +1,8 @@
 #include "../headers/exceptii.hpp"
 
+///Cazul in care un proiectil are ca tinta un inamic cu un index invalid.
 const char* exceptionProjectileIndex::what() const noexcept{return "TargetIndex invalid!";}
 
+///Cazul in care un turn este pus in afara grid-ului.
 const char* exceptionTowerPosition::what() const noexcept{return "TowerPosition invalid!";}
+
